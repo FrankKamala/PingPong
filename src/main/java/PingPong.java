62dcabda931bdb92a47eae864e0af8488b2aebe4
FrankKamala <frankkamala@gmail.com>
2019-12-13 11:32:24 +0300

    Initial commit with test file

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PingPong {
+    public static void main(String[] args) {
+//        Scanner myScanner = new Scanner(System.in);
+//
+//        System.out.println("Enter number gazee");
+//        Integer number = myScanner.nextInt();
+//
+//        if (number %3==0){
+//            System.out.println("Ping" );
+//        }else if(number%5==0){
+//            System.out.println("Pong" );
+//        }else if (number %3==0 && number%5==0){
+//            System.out.println("PingPong" );
+//        }
+
+
+    }
+}
