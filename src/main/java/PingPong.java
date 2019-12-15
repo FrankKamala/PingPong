35ab96e3d08d1c72bd1a285d2e7e46d4ff57bc87
FrankKamala <frankkamala@gmail.com>
2019-12-15 15:12:52 +0300

    add second test

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class PingPong {
-    public static void main(String[] args) {
-//        Scanner myScanner = new Scanner(System.in);
-//
-//        System.out.println("Enter number gazee");
-//        Integer number = myScanner.nextInt();
-//
-//        if (number %3==0){
-//            System.out.println("Ping" );
-//        }else if(number%5==0){
-//            System.out.println("Pong" );
-//        }else if (number %3==0 && number%5==0){
-//            System.out.println("PingPong" );
-//        }
-
-
+    public ArrayList<Object> runPingPong(int countUpTo){
+        ArrayList<Object> result = new ArrayList<Object>();
+        result.add(1);
+        return result;
     }
+//    public static void main(String[] args) {
+////        Scanner myScanner = new Scanner(System.in);
+////
+////        System.out.println("Enter number gazee");
+////        Integer number = myScanner.nextInt();
+////
+////        if (number %3==0){
+////            System.out.println("Ping" );
+////        }else if(number%5==0){
+////            System.out.println("Pong" );
+////        }else if (number %3==0 && number%5==0){
+////            System.out.println("PingPong" );
+////        }
+//
+//
+//    }
 }
