7f0feb390a62b7f7ab1a74fd55f2004ac2ac02b8
FrankKamala <frankkamala@gmail.com>
2019-12-15 15:36:24 +0300

    add  testReplace Multiple of 5 and 5and3

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -4,7 +4,17 @@ public class PingPong {
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
      for (int i = 1; i<= countUpTo; i++){
-         result.add(i);
+         if (i%3 ==0){
+             result.add("ping");
+         }else if (i%5==0)
+         {
+             result.add("pong");
+         }
+
+         else{
+             result.add(i);
+         }
+
         }
 
         return result;
